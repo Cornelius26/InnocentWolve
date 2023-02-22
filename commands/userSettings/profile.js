14b3776f2d6b63ca44a9ea8ec934eb57524560b9
CR <mail@corneliusreh.com>
2023-02-22 16:15:26 +0100

    bug fix

--- commands/userSettings/profile.js
+++ commands/userSettings/profile.js
@@ -38,6 +38,7 @@ export const execute = async (interaction) => {
 	try {
 		// A outsourced often used function that checks the user access permissions
 		checkAccessRight(interaction).then(rights => {
+			console.log(rights);
 			const access = rights[0];
 			const userData = rights[2];
 			if (access != 'clanNotRegistered' && access != 'noAccess') {
@@ -50,8 +51,7 @@ export const execute = async (interaction) => {
 				interaction.editReply({ content: 'You have no access to this command.', ephemeral: true });
 			}
 		});
-	}
-	catch (e) {
+	} catch (e) {
 		console.log(e);
 		interaction.editReply({
 			content: 'You have no access to the bot or a internal error accured.',
@@ -69,79 +69,79 @@ export const execute = async (interaction) => {
  */
 const profileImage = async (clanMember, wolvesvilleMemberData) => {
 
-	const url = wolvesvilleMemberData.equippedAvatar.url.slice(0, -4) + '@3x' + wolvesvilleMemberData.equippedAvatar.url.slice(-4);
-	return loadImage(url).then(image => {
-		return loadImage('https://cdn.wolvesville.com/backgrounds/wolvesville_large_day.wide@2x.png').then(backgroundimage => {
-			const neededHeight = image.height;
-			const maxWidthImages = image.width;
+		const url = wolvesvilleMemberData.equippedAvatar.url.slice(0, -4) + '@3x' + wolvesvilleMemberData.equippedAvatar.url.slice(-4);
+		return loadImage(url).then(image => {
+			return loadImage('https://cdn.wolvesville.com/backgrounds/wolvesville_large_day.wide@2x.png').then(backgroundimage => {
+				const neededHeight = image.height;
+				const maxWidthImages = image.width;
 
 
-			const canvas = createCanvas(backgroundimage.width+300, neededHeight);
-			const getTextWidth = (text, font) => {
-				// if given, use cached canvas for better performance
-				// else, create new canvas
-				const localContext = canvas.getContext('2d');
-				localContext.font = font;
-				const metrics = localContext.measureText(text);
-				return metrics.width;
-			};
+				const canvas = createCanvas(backgroundimage.width + 300, neededHeight);
+				const getTextWidth = (text, font) => {
+					// if given, use cached canvas for better performance
+					// else, create new canvas
+					const localContext = canvas.getContext('2d');
+					localContext.font = font;
+					const metrics = localContext.measureText(text);
+					return metrics.width;
+				};
 
 
-			const context = canvas.getContext('2d');
-			context.fillStyle = '#e2e2e2';
-			context.fillRect(0, 0, canvas.width, canvas.height);
-			context.drawImage(backgroundimage, -20, (canvas.height - backgroundimage.height) / 2, backgroundimage.width, backgroundimage.height);
-			context.fillStyle = 'rgba(24,24,24,0.4)';
-			context.fillRect(image.width, 0, canvas.width - image.width, canvas.height);
-			context.fillStyle = '#ffffff';
-			// context.fillRect(1, 1, image.width, image.height);
-			context.drawImage(image, 0, 0, image.width, image.height);
-			context.strokeStyle = '#000000';
+				const context = canvas.getContext('2d');
+				context.fillStyle = '#e2e2e2';
+				context.fillRect(0, 0, canvas.width, canvas.height);
+				context.drawImage(backgroundimage, -20, (canvas.height - backgroundimage.height) / 2, backgroundimage.width, backgroundimage.height);
+				context.fillStyle = 'rgba(24,24,24,0.4)';
+				context.fillRect(image.width, 0, canvas.width - image.width, canvas.height);
+				context.fillStyle = '#ffffff';
+				// context.fillRect(1, 1, image.width, image.height);
+				context.drawImage(image, 0, 0, image.width, image.height);
+				context.strokeStyle = '#000000';
 
-			const outerMarginSize = image.height * 0.06;
-			const innerMarginSize = image.height * 0.04;
-			const titleSize = image.height * 0.18;
-			const balanceSize = image.height * 0.15;
-			const donatedSize = image.height * 0.11;
-			const leftMarginSize = maxWidthImages * 0.10;
-			const titlePosition = outerMarginSize + titleSize;
-			const balancePosition1 = titlePosition + outerMarginSize + balanceSize;
-			const balancePosition2 = balancePosition1 + innerMarginSize + balanceSize;
-			const donatedPosition1 = balancePosition2 + innerMarginSize + donatedSize;
-			const donatedPosition2 = donatedPosition1 + innerMarginSize + donatedSize;
+				const outerMarginSize = image.height * 0.06;
+				const innerMarginSize = image.height * 0.04;
+				const titleSize = image.height * 0.18;
+				const balanceSize = image.height * 0.15;
+				const donatedSize = image.height * 0.11;
+				const leftMarginSize = maxWidthImages * 0.10;
+				const titlePosition = outerMarginSize + titleSize;
+				const balancePosition1 = titlePosition + outerMarginSize + balanceSize;
+				const balancePosition2 = balancePosition1 + innerMarginSize + balanceSize;
+				const donatedPosition1 = balancePosition2 + innerMarginSize + donatedSize;
+				const donatedPosition2 = donatedPosition1 + innerMarginSize + donatedSize;
 
-			context.font = process.env.ENVIROMENT == 'production' ? `${titleSize}px DejaVu Sans` : `${titleSize}px sans-serif`;
-			context.fillStyle = '#ffffff';
-			context.fillText(wolvesvilleMemberData.username, image.width + leftMarginSize, titlePosition);
+				context.font = process.env.ENVIROMENT == 'production' ? `${titleSize}px DejaVu Sans` : `${titleSize}px sans-serif`;
+				context.fillStyle = '#ffffff';
+				context.fillText(wolvesvilleMemberData.username, image.width + leftMarginSize, titlePosition);
 
-			context.font = process.env.ENVIROMENT == 'production' ? `${balanceSize}px Noto Color Emoji` : `${balanceSize}px sans-serif`;
-			context.fillText('💰', image.width + leftMarginSize, balancePosition1);
-			context.fillText('💎', image.width + leftMarginSize, balancePosition2);
-			const balanceEmojiWidth1 = getTextWidth('💰 ', context.font);
-			const balanceEmojiWidth2 = getTextWidth('💎 ', context.font);
+				context.font = process.env.ENVIROMENT == 'production' ? `${balanceSize}px Noto Color Emoji` : `${balanceSize}px sans-serif`;
+				context.fillText('💰', image.width + leftMarginSize, balancePosition1);
+				context.fillText('💎', image.width + leftMarginSize, balancePosition2);
+				const balanceEmojiWidth1 = getTextWidth('💰 ', context.font);
+				const balanceEmojiWidth2 = getTextWidth('💎 ', context.font);
 
-			context.font = process.env.ENVIROMENT == 'production' ? `${balanceSize}px DejaVu Sans` : `${balanceSize}px sans-serif`;
-			context.fillText('Current Gold Balance ' + clanMember.goldBalance.toString(), image.width + balanceEmojiWidth1, balancePosition1);
-			context.fillText('Current Gem Balance ' + clanMember.gemsBalance.toString(), image.width + balanceEmojiWidth2, balancePosition2);
+				context.font = process.env.ENVIROMENT == 'production' ? `${balanceSize}px DejaVu Sans` : `${balanceSize}px sans-serif`;
+				context.fillText('Current Gold Balance ' + clanMember.goldBalance.toString(), image.width + balanceEmojiWidth1, balancePosition1);
+				context.fillText('Current Gem Balance ' + clanMember.gemsBalance.toString(), image.width + balanceEmojiWidth2, balancePosition2);
 
-			context.font = process.env.ENVIROMENT == 'production' ? `${donatedSize}px Noto Color Emoji` : `${donatedSize}px sans-serif`;
-			context.fillText('💰', image.width + leftMarginSize, donatedPosition1);
-			context.fillText('💎', image.width + leftMarginSize, donatedPosition2);
-			const donatedEmojiWidth1 = getTextWidth('💰 ', context.font);
-			const donatedEmojiWidth2 = getTextWidth('💎 ', context.font);
-			context.font = process.env.ENVIROMENT == 'production' ? `${donatedSize}px DejaVu Sans` : `${donatedSize}px sans-serif`;
-			context.fillText('Total Gold Donated ' + clanMember.goldDonated.toString(), image.width + donatedEmojiWidth1, donatedPosition1);
-			context.fillText('Total Gems Donated ' + clanMember.gemsDonated.toString(), image.width + donatedEmojiWidth2, donatedPosition2);
+				context.font = process.env.ENVIROMENT == 'production' ? `${donatedSize}px Noto Color Emoji` : `${donatedSize}px sans-serif`;
+				context.fillText('💰', image.width + leftMarginSize, donatedPosition1);
+				context.fillText('💎', image.width + leftMarginSize, donatedPosition2);
+				const donatedEmojiWidth1 = getTextWidth('💰 ', context.font);
+				const donatedEmojiWidth2 = getTextWidth('💎 ', context.font);
+				context.font = process.env.ENVIROMENT == 'production' ? `${donatedSize}px DejaVu Sans` : `${donatedSize}px sans-serif`;
+				context.fillText('Total Gold Donated ' + clanMember.goldDonated.toString(), image.width + donatedEmojiWidth1, donatedPosition1);
+				context.fillText('Total Gems Donated ' + clanMember.gemsDonated.toString(), image.width + donatedEmojiWidth2, donatedPosition2);
 
 
-			return new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile-image.png' });
+				return new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile-image.png' });
+
+			});
 
 		});
 
-	});
 
-
-}
+	}
 ;
 
 /**
